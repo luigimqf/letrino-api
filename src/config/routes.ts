d648c0b19f1b90377499d52c3738c1f10261643c
Luigi Mendes <luigi.jackson@gmail.com>
2025-08-13 14:14:34 -0300

    chore: bug

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -5,6 +5,7 @@ import { authenticate } from '../middlewares/authenticate'
 import { sentryUserContext } from '../middlewares/sentry'
 import { getWord, attemptFail, registerSkippedAttemp,deleteSkippedAttempDocument, attemptSuccess } from '../controllers/word.controller'
 import { checkAttempts } from '../middlewares/attempts'
+import { env } from './enviroment'
 
 function setupRoutes(app: Express) {
   // Middleware global do Sentry para contexto
@@ -27,7 +28,22 @@ function setupRoutes(app: Express) {
 
   app.get('/debug', async (req, res) => {
     throw new Error('Debugging error')
-  })
+  });
+
+  app.get('/env', async (req, res) => {
+    res.status(200).json({
+      NODE_ENV: env.NODE_ENV,
+      PORT: env.NODE_ENV,
+      MONGODB_URI: env.NODE_ENV,
+      JWT_SECRET: env.NODE_ENV,
+      EMAIL_USER: env.NODE_ENV,
+      EMAIL_PASSWORD: env.NODE_ENV,
+      ALLOWED_ORIGIN: env.NODE_ENV,
+      SENTRY_DSN: env.NODE_ENV,
+      PASSWORD_RESET_URL: env.NODE_ENV,
+    })
+  });
+
   //----------- Auth Routes ------------//
   app.post('/sign-in', signIn);
 
