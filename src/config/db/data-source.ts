4ac26b9b64bd3f3f846683759a6f18347f200489
Luigi Mendes <luigi.jackson@gmail.com>
2025-10-05 15:18:46 -0300

    refactor: update data source synchronization setting for development environment and remove obsolete migration files

--- src/config/db/data-source.ts
+++ src/config/db/data-source.ts
@@ -5,7 +5,7 @@ import { Attempt, Match, UsedWord, User, Word } from './entity';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: env.DB_URL,
-  synchronize: false,
+  synchronize: env.NODE_ENV === 'development',
   logging: env.NODE_ENV === 'development',
   entities: [User, Word, UsedWord, Match, Attempt],
   migrations: ['src/config/db/migrations/*.ts'],
